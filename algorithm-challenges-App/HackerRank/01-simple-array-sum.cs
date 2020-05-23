c8e313e3172646c24c798ba7517f0ae6e4aa654e
Jean Alejandro Pico <jealepive@hotmail.com>
2020-05-23 15:20:34 -0500

    Acumulado en un arreglo de interos

---
+++ algorithm-challenges-App/HackerRank/01-simple-array-sum.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace algorithm_challenges_App.HackerRank
+{
+    public class Solution
+    {
+        /*
+         * Complete the simpleArraySum function below.
+         */
+        public static int simpleArraySum(int[] ar)
+        {
+            return ar.Sum();
+        }
+
+    }
+}
---
+++ algorithm-challenges-App/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using algorithm_challenges_App.HackerRank;
+
+namespace algorithm_challenges_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution.simpleArraySum(new int[] { 1, 2, 3 });
+        }
+    }
+}
