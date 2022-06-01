face41d99b39944dace085835e7996249b63778e
Jean Alejandro Pico <jealepive@hotmail.com>
2022-05-31 23:30:29 -0500

    LeetCode - 001 Two Sum challenge

---
+++ LeetCode.Tests/001-TwoSum-Tests.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode.Tests
+{
+    public class _001_TwoSum_Tests
+    {
+        [Theory]
+        [InlineData(new[] { 2, 7, 11, 15 }, 9, new[] { 0, 1 })]
+        [InlineData(new[] { 3, 2, 4 }, 6, new[] { 1, 2 })]
+        [InlineData(new[] { 3, 3 }, 6, new[] { 0, 1 })]
+        public void TwoSum_Examples(int[] input, int target, int[] output)
+        {
+            // Arrange
+            var testObj = new _001_TwoSum();
+
+            // Act
+            var result = testObj.TwoSum(input, target);
+
+            // Assert
+            Assert.Equal(output, result);
+        }
+    }
+}
---
+++ LeetCode/001-TwoSum.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode
+{
+    /*
+     *   Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+     *   You may assume that each input would have exactly one solution, and you may not use the same element twice.
+     *   You can return the answer in any order.
+    */
+
+    public class _001_TwoSum
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target && i != j)
+                    {
+                        return new[] { i, j };
+                    }
+                }
+            }
+
+            return new[] { 0, 0 };
+        }
+    }
+}
