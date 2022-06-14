9e40723d197d37f9800d0c99eeb1fba3e4d6821f
Jean Alejandro Pico <jealepive@hotmail.com>
2022-06-13 23:42:33 -0500

    LeetCode - 003 Add Length of Longest Substring challenge

---
+++ LeetCode.Tests/003-LengthOfLongestSubstring-Tests.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode.Tests
+{
+    public class _003_LengthOfLongestSubstring_Tests
+    {
+        [Theory]
+        [InlineData("abcabcbb", 3)]
+        [InlineData("bbbbb", 1)]
+        [InlineData("pwwkew", 3)]
+        [InlineData("  ", 1)]
+        [InlineData("a", 1)]
+        [InlineData("au", 2)]
+        [InlineData("aab", 2)]
+        [InlineData("dvdf", 3)]
+        [InlineData("anviaj", 5)]
+        [InlineData("asljlj", 4)]
+        [InlineData("nfpdmpi", 5)]
+        public void LengthOfLongestSubstring_Examples(string input, int output)
+        {
+            // Arrange
+            var testObj = new _003_LengthOfLongestSubstring();
+
+            // Act
+            var result = testObj.LengthOfLongestSubstring(input);
+
+            // Assert
+            Assert.Equal(output, result);
+        }
+    }
+}
---
+++ LeetCode/Problems/003-LengthOfLongestSubstring.cs
@@ -0,0 +1,73 @@
+﻿namespace LeetCode.Problems
+{
+    public class _003_LengthOfLongestSubstring
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return 0;
+            }
+
+            if (s.Length is 1)
+            {
+                return 1;
+            }
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] != s[0])
+                {
+                    break;
+                }
+
+                if (i == s.Length - 1)
+                {
+                    return 1;
+                }
+            }
+
+            var longestSubstring = 0;
+            var lastElementIndex = s.Length - 1;
+            var currentSubString = new List<char>();
+            var lastDuplicatedElementIndex = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!currentSubString.Contains(s[i]) && i != lastElementIndex)
+                {
+                    currentSubString.Add(s[i]);
+                }
+                else if (!currentSubString.Contains(s[i]) && i == lastElementIndex)
+                {
+                    currentSubString.Add(s[i]);
+                    if (currentSubString.Count > longestSubstring)
+                    {
+                        longestSubstring = currentSubString.Count;
+                    }
+                }
+                else if (currentSubString.Contains(s[i]) && i == lastElementIndex)
+                {
+                    if (currentSubString.Count > longestSubstring)
+                    {
+                        longestSubstring = currentSubString.Count;
+                    }
+
+                    break;
+                }
+                else
+                {
+                    i = s.IndexOf(s[i], startIndex: lastDuplicatedElementIndex);
+                    lastDuplicatedElementIndex = i;
+
+                    if (currentSubString.Count > longestSubstring)
+                    {
+                        longestSubstring = currentSubString.Count;
+                        currentSubString = new List<char>();
+                    }
+                }
+            }
+            return longestSubstring;
+        }
+    }
+}
