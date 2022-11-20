6fd9487e0cdfde4db7d35e5173f3ffeb0ed3d9ca
Jean Alejandro Pico <jealepive@hotmail.com>
2022-11-20 17:51:15 -0500

    Improve performance to 009 PalindromeNumber

--- LeetCode/Problems/009-PalindromeNumber.cs
+++ LeetCode/Problems/009-PalindromeNumber.cs
@@ -3,9 +3,9 @@
 /*
  *  Given an integer x, return true if x is a palindrome, and false otherwise. 
  *  
- *  Runtime: 85 ms
+ *  Runtime: 39 ms
  *  Memory Usage: 30 MB
- *  Submission: https://leetcode.com/submissions/detail/846696749/
+ *  Submission: https://leetcode.com/submissions/detail/847126844/
 */
 
 public class _009_PalindromeNumber
@@ -13,7 +13,20 @@ public class _009_PalindromeNumber
     public static bool IsPalindrome(int x)
     {
         var input = x.ToString();
-        var temp = new string((char[]?)input.Reverse().ToArray());
-        return temp.Equals(input);
+        var i = 0;
+        var isPalindrome = false;
+
+        while (i != input.Length - (1 / 2))
+        {
+            isPalindrome = input[i] == input[input.Length - 1 - i];
+            if (!isPalindrome)
+            {
+                return isPalindrome;
+            }
+
+            i++;
+        }
+
+        return isPalindrome;
     }
 }
