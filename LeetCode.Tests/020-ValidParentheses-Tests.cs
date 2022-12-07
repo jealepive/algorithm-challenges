5f0e1f5921ffe12f3db3ec0b1298ad575ba9bf83
Jean Alejandro Pico <jealepive@hotmail.com>
2022-12-06 21:10:52 -0500

    Add 020 Valid Parentheses - LeetCode

---
+++ LeetCode.Tests/020-ValidParentheses-Tests.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.Tests;
+
+public class _020_ValidParentheses_Tests
+{
+    [Theory]
+    [InlineData("()", true)]
+    [InlineData("()[]{}", true)]
+    [InlineData("(]", false)]
+    [InlineData("([)]", false)]
+    [InlineData("([{}])", true)]
+    [InlineData("((", false)]
+    [InlineData("(", false)]
+    [InlineData("){", false)]
+    public void ValidParentheses_Examples(string input, bool expected)
+    {
+        // Act
+        var actual = _020_ValidParentheses.IsValid(input);
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ LeetCode/Problems/020-ValidParentheses.cs
@@ -0,0 +1,47 @@
+﻿namespace LeetCode.Problems;
+
+public class _020_ValidParentheses
+{
+    private static readonly char[] openBrackets = { '(', '[', '{' };
+    private static readonly char[] closeBrackets = { ')', ']', '}' };
+    private static readonly Dictionary<char, char> mapBrackets = new()
+    {
+        { '(', ')' },
+        { '[', ']' },
+        { '{', '}' }
+    };
+    public static bool IsValid(string s)
+    {
+        if (s.Length <= 1)
+        {
+            return false;
+        }
+
+        if (closeBrackets.Contains(s[0]))
+        {
+            return false;
+        }
+
+        var isValid = true;
+        var stack = new Stack<char>();
+
+        for (var i = 0; i < s.Length; i++)
+        {
+            if (openBrackets.Contains(s[i]))
+            {
+                stack.Push(s[i]);
+            }
+            else
+            {
+                var hasElements = stack.TryPop(out var pendingToClose);
+                if (!hasElements || mapBrackets[pendingToClose] != s[i])
+                {
+                    isValid = false;
+                    break;
+                }
+            }
+        }
+
+        return isValid && stack.Count == 0;
+    }
+}
