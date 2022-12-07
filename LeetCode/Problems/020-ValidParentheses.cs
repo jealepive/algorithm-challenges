8baf96da6d62aa62fba44d2e75e97d663ef66363
Jean Alejandro Pico <jealepive@hotmail.com>
2022-12-06 21:12:58 -0500

    Add description to 020 Valid Parentheses - LeetCode

--- LeetCode/Problems/020-ValidParentheses.cs
+++ LeetCode/Problems/020-ValidParentheses.cs
@@ -1,5 +1,20 @@
 ﻿namespace LeetCode.Problems;
 
+/*
+ *  Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+ *
+ *  An input string is valid if:
+ *  
+ *  Open brackets must be closed by the same type of brackets.
+ *  Open brackets must be closed in the correct order.
+ *  Every close bracket has a corresponding open bracket of the same type.
+ * 
+ * 
+ * Runtime: 78 ms
+ * Memory Usage: 38 MB
+ * Submission: https://leetcode.com/problems/valid-parentheses/submissions/855859596/
+ * 
+ */
 public class _020_ValidParentheses
 {
     private static readonly char[] openBrackets = { '(', '[', '{' };
